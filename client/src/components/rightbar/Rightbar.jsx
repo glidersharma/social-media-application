3bb7bcf5cb64f1cd7b3bfbb01d673618a4756fa5
PiyushB621 <piyushbhandari621@gmail.com>
2021-06-14 02:38:39 +0530

    images

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -170,7 +170,7 @@ export default function Rightbar({ user }) {
         <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+            <b>Tanish Sharma</b> and <b>2 other friends</b> have a birhday today.
           </span>
         </div>
         <img className="rightbarAd" src="assets/ad.png" alt="" />

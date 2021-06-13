18b3d5e91f197ff826d3368c7d1730e10d4bdab9
PiyushB621 <piyushbhandari621@gmail.com>
2021-06-14 03:16:10 +0530

    Profile.jsx

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -71,21 +71,17 @@ export default function Profile() {
             <h4 className="rightbarTitle">User information</h4>
             <div className="rightbarInfo">
               <div className="rightbarInfoItem">
-                <span className="rightbarInfoKey">City: Delhi</span>
+                <span className="rightbarInfoKey">City: </span>
                 <span className="rightbarInfoValue">{user.city}</span>
               </div>
               <div className="rightbarInfoItem">
-                <span className="rightbarInfoKey">From: Mumbai</span>
+                <span className="rightbarInfoKey">From: </span>
                 <span className="rightbarInfoValue">{user.from}</span>
               </div>
               <div className="rightbarInfoItem">
-                <span className="rightbarInfoKey">Relationship: Single</span>
+                <span className="rightbarInfoKey">Relationship: </span>
                 <span className="rightbarInfoValue">
-                  {user.relationship === 1
-                    ? "Single"
-                    : user.relationship === 1
-                      ? "Married"
-                      : ""}
+                  {user.relationship}
                 </span>
               </div>
               <div className="rightbarInfoItem">
